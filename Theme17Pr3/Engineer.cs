7833556ce411bbde273280b4669e5e197647d663
D1oBrand00 <122979732+D1oBrand00@users.noreply.github.com>
2024-02-16 13:21:52 +0200

    Update Engineer.cs

--- Theme17Pr3/Engineer.cs
+++ Theme17Pr3/Engineer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,54 @@ using System.Threading.Tasks;
 
 namespace Theme17Pr3
 {
-    /// <summary>
-    /// Производный класс от Employee
-    /// </summary>
     class Engineer : Employee
     {
+        /// <summary>
+        /// Поле кол-ва разработанных проектов
+        /// </summary>
+        int n;
+        /// <summary>
+        /// Свойства поля
+        /// </summary>
+        public int N { get => n; set => n = value; }
+        /// <summary>
+        /// Конструктор производного класса с 5 параметрами
+        /// </summary>
+        /// <param name="name">Имя</param>
+        /// <param name="surname">Фамилия</param>
+        /// <param name="birthDate">Дата рождения</param>
+        /// <param name="p">Минимальная зарплата</param>
+        /// <param name="n">Кол-во разработанных проектов</param>
+        public Engineer (string name, string surname, DateTime birthDate, int p, int n) : base(name, surname, birthDate, p)
+        {
+            this.n = n;
+        }
+        /// <summary>
+        /// Метод изменения дохода на 4.8*n
+        /// </summary>
+        /// <param name="k"></param>
+        /// <param name="H"></param>
+        /// <returns></returns>
+        public override double Income(double k, double H)
+        {
+            return base.Income(k, H) + 4.8 * n;
+        }
+        /// <summary>
+        /// Метод вывода ифнормации
+        /// </summary>
+        public override void Output()
+        {
+            base.Output();
+            Console.WriteLine($"Кол-во разработанных проектов - {n}\nДоход менеджера увеличить на 4.8*n - {Income}");
+        }
+
+
+
+
+
+
     }
+
+        
+    
 }
